cf2d2a0cb75337bcb7eba2949a386ce69fd59e5d
gYunTian <54625060+gYunTian@users.noreply.github.com>
2020-03-26 18:32:25 +0800

    two orchestrators

--- microservices/control.py
+++ microservices/control.py
@@ -2,59 +2,41 @@ from flask import Flask, jsonify, make_response
 import json
 from flask_cors import CORS
 import requests
+
 app = Flask(__name__)
 
 
-@app.route('/get/<ticker>')
-def get_articles(ticker):
+@app.route('/get/<string: ticker>')
+def send_ticker(ticker):
+    #should only orchestrate for Alex's, stock news, indicator
+
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    ticker = 'AAPL'
     
-    #initialize json to return
+    #initialize json object to return
     return_arr = {
         "Stock price": "",
-        "General sentiment": "",
         "Stock sentiment": "",
-        "VIX": "",
         "RSI": "",
         "MACD_S": "",
         "MACD": ""
     }
     
-    #Alex stock details
-    #detail = requests.get(url, headers=headers)
-
-
-
-
     #parallel 1
-    #general news sentiment
-    general = requests.get('http://localhost:5003/scrape', headers=headers)
-    result = json.loads(general.text)
-    
-    #sentiment service
-    sentiment = requests.post('http://localhost:5004/sentiment', json=result, headers=headers).text
-    sentiment = str(round(float(sentiment),2))+"%"
-    
-    return_arr["General sentiment"] = sentiment
+    #Alex stock details
+    #detail = requests.get(url+ticker, headers=headers)
 
-    
+
+    #parallel 2
     #stock sentiment
     stock = requests.get('http://localhost:5006/scrape/'+ticker, headers=headers)
     result = json.loads(stock.text)
     
-    sentiment2 = requests.post('http://localhost:5004/sentiment', json=result, headers=headers).text
-    sentiment2 += str(round(float(sentiment2),2))+"%"
+    sentiment = requests.post('http://localhost:5004/sentiment', json=result, headers=headers).text
+    sentiment += str(round(float(sentiment),2))+"%"
     
-    return_arr["Stock sentiment"] = sentiment2
+    return_arr["Stock sentiment"] = sentiment
     
-
-    #VIX 
-    vix = requests.get('http://localhost:5007/vix', headers=headers)
-    result = json.loads(vix.text)
-    return_arr["VIX"] = result
-
-    
+    #parallel 3
     #indicators
     indicators = requests.get('http://localhost:5007/vix', headers=headers)
     indicators = json.loads(indicators.text)
@@ -69,8 +51,7 @@ def get_articles(ticker):
     return indicators, 200   
 
     
-    #lcaolhost:5005/get/ticker
 if __name__ == "__main__":
-    app.run(host='localhost', port=5005, debug=True)
+    app.run(host='localhost', port=5002, debug=True)
 
 
---
+++ microservices/control2.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, make_response
+import json
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+
+@app.route('/get/<string: ticker>')
+def send_ticker(ticker):
+    #VIX, general sentiment
+
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    
+    #initialize json to return
+    return_arr = {
+        "Genral sentiment": "",
+        "VIX": ""
+    }
+
+    #parallel 1
+    #general news sentiment
+    general = requests.get('http://localhost:5003/scrape', headers=headers)
+    result = json.loads(general.text)
+
+    #sentiment service
+    sentiment = requests.post('http://localhost:5004/sentiment', json=result, headers=headers).text
+    sentiment = str(round(float(sentiment),2))+"%"
+    
+    return_arr["General sentiment"] = sentiment
+
+
+    #VIX 
+    vix = requests.get('http://localhost:5007/vix', headers=headers)
+    result = json.loads(vix.text)
+    return_arr["VIX"] = result
+
+    return return_arr, 200   
+
+    
+    #lcaolhost:5005/get/ticker
+if __name__ == "__main__":
+    app.run(host='localhost', port=5001, debug=True)
+
+
